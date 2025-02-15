5f7a04653c3a86c0e358815515da6aca70aa63bf
thevithach <vitt@uia.no>
2025-02-15 17:13:32 +0100

    config fix

--- geonorge-server/src/config.py
+++ geonorge-server/src/config.py
@@ -25,5 +25,8 @@ CONFIG = {
         "azure_gpt_endpoint": os.getenv("AZURE_GPT_ENDPOINT", ""),  
         "azure_embedding_api_key": os.getenv("AZURE_EMBEDDING_API_KEY"),
         "azure_embeddings_endpoint": "https://kartai-openai.openai.azure.com/openai/deployments/text-embedding-3-large/embeddings?api-version=2023-05-15", 
+        "gemini_api_key": os.getenv("GEMINI_API_KEY"),
+        "gemini_base_endpoint": os.getenv("GEMINI_BASE_ENDPOINT"),
+        "gemini_full_endpoint": os.getenv("GEMINI_FULL_ENDPOINT"),
     },
 }
