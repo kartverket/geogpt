b2c17bd420059c169518ace62466e07de7e951c0
Nhan Thomas Trong Nguyen <nt.thom.ng@gmail.com>
2025-03-27 19:33:47 +0100

    fix: fixed search layer and code structure
    fix: small layer and dataset type fix

--- geonorge-app/components/sidebar_components/BaseMapSelector.tsx
+++ geonorge-app/components/sidebar_components/BaseMapSelector.tsx
@@ -1,59 +1,69 @@
 import * as React from "react";
-import Image from "next/image";
 import { BaseMapButton } from "@/components/sidebar_components/BaseMapButton";
-import {TranslationKey} from "@/i18n/translations";
+import { TranslationKey } from "@/i18n/translations";
 
 interface BaseMapSelectorProps {
-    selectedBaseMap: string;
-    onChangeBaseLayer: {
-        revertToBaseMap: () => void;
-        changeToGraattKart: () => void;
-        changeToRasterKart: () => void;
-    };
-    t: (key: TranslationKey) => string;
-    setSelectedBaseMap: (map: string) => void;
+  selectedBaseMap: string;
+  onChangeBaseLayer: {
+    revertToBaseMap: () => void;
+    changeToGraattKart: () => void;
+    changeToRasterKart: () => void;
+    changeToSjoKart: () => void;
+  };
+  t: (key: TranslationKey) => string;
+  setSelectedBaseMap: (map: string) => void;
 }
 
 export const BaseMapSelector: React.FC<BaseMapSelectorProps> = ({
-                                                                    selectedBaseMap,
-                                                                    onChangeBaseLayer,
-                                                                    t,
-                                                                    setSelectedBaseMap,
-                                                                }) => {
-    return (
-        <div className="grid grid-cols-3 gap-2">
-            <BaseMapButton
-                selected={selectedBaseMap === "landskart"}
-                onClick={() => {
-                    setSelectedBaseMap("landskart");
-                    onChangeBaseLayer.revertToBaseMap();
-                }}
-                imageSrc="https://norgeskart.no/assets/img/land.png"
-                altText="Landskap kart"
-                label={t("landscape_map")}
-            />
+  selectedBaseMap,
+  onChangeBaseLayer,
+  t,
+  setSelectedBaseMap,
+}) => {
+  return (
+    <div className="grid grid-cols-2 gap-2">
+      <BaseMapButton
+        selected={selectedBaseMap === "landskart"}
+        onClick={() => {
+          setSelectedBaseMap("landskart");
+          onChangeBaseLayer.revertToBaseMap();
+        }}
+        imageSrc="https://norgeskart.no/assets/img/land.png"
+        altText="Landskap kart"
+        label={t("landscape_map")}
+      />
 
-            <BaseMapButton
-                selected={selectedBaseMap === "graatone"}
-                onClick={() => {
-                    setSelectedBaseMap("graatone");
-                    onChangeBaseLayer.changeToGraattKart();
-                }}
-                imageSrc="https://norgeskart.no/assets/img/grey.png"
-                altText="Gråtone kart"
-                label={t("grayscale_map")}
-            />
+      <BaseMapButton
+        selected={selectedBaseMap === "graatone"}
+        onClick={() => {
+          setSelectedBaseMap("graatone");
+          onChangeBaseLayer.changeToGraattKart();
+        }}
+        imageSrc="https://norgeskart.no/assets/img/grey.png"
+        altText="Gråtone kart"
+        label={t("grayscale_map")}
+      />
 
-            <BaseMapButton
-                selected={selectedBaseMap === "rasterkart"}
-                onClick={() => {
-                    setSelectedBaseMap("rasterkart");
-                    onChangeBaseLayer.changeToRasterKart();
-                }}
-                imageSrc="https://norgeskart.no/assets/img/raster.png"
-                altText="Raster kart"
-                label={t("raster_map")}
-            />
-        </div>
-    );
+      <BaseMapButton
+        selected={selectedBaseMap === "rasterkart"}
+        onClick={() => {
+          setSelectedBaseMap("rasterkart");
+          onChangeBaseLayer.changeToRasterKart();
+        }}
+        imageSrc="https://norgeskart.no/assets/img/raster.png"
+        altText="Raster kart"
+        label={t("raster_map")}
+      />
+      <BaseMapButton
+        selected={selectedBaseMap === "sjokart"}
+        onClick={() => {
+          setSelectedBaseMap("sjokart");
+          onChangeBaseLayer.changeToSjoKart();
+        }}
+        imageSrc="https://norgeskart.no/assets/img/dummy.png"
+        altText="Sjø kart"
+        label={t("sea_map")}
+      />
+    </div>
+  );
 };
