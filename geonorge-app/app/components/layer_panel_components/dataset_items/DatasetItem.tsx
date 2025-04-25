8b6543f3dadd8661855486634b8f4c89cb7c7eaf
thevithach <vitt@uia.no>
2025-04-25 14:23:30 +0200

    hover/text fix in dataset (kartkatalog),  hover and link wont trigger unless you hover the text and not the background around it.

--- geonorge-app/app/components/layer_panel_components/dataset_items/DatasetItem.tsx
+++ geonorge-app/app/components/layer_panel_components/dataset_items/DatasetItem.tsx
@@ -195,27 +195,27 @@ const DatasetItem: React.FC<DatasetItemProps> = (props) => {
               open={isHoverCardOpen}
               onOpenChange={handleHoverCardOpenChange}
             >
-              <HoverCardTrigger asChild>
-                <a
-                  href={`https://kartkatalog.geonorge.no/metadata/${encodeURIComponent(
-                    searchResult.title || "Dataset"
-                  )}/${resultKey}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  onClick={(e) => e.stopPropagation()}
-                  className="text-sm font-medium transition-colors duration-200 underline-offset-4 hover:underline block"
-                >
-                  <span
-                    className={`${
-                      hasActiveLayer
-                        ? "text-color-gn-primary"
-                        : "text-gray-800 hover:text-color-gn-lightblue"
-                    } line-clamp-2`}
+              <span
+                className={`${
+                  hasActiveLayer
+                    ? "text-color-gn-primary"
+                    : "text-gray-800 hover:text-color-gn-lightblue"
+                } line-clamp-2 text-sm`}
+              >
+                <HoverCardTrigger asChild>
+                  <a
+                    href={`https://kartkatalog.geonorge.no/metadata/${encodeURIComponent(
+                      searchResult.title || "Dataset"
+                    )}/${resultKey}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={(e) => e.stopPropagation()}
+                    className="underline-offset-4 hover:underline"
                   >
                     {searchResult.title || "Ukjent Tittel"}
-                  </span>
-                </a>
-              </HoverCardTrigger>
+                  </a>
+                </HoverCardTrigger>
+              </span>
               <HoverCardContent
                 side="left"
                 className="w-80 p-4 rounded-omar border border-gray-200 shadow-lg"
