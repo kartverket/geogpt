06e13d0ae78a55c06352f6cde4f85920d7061597
thevithach <vitt@uia.no>
2025-05-27 14:58:09 +0200

    test

--- geonorge-server/tests/test_api_endpoints.py
+++ geonorge-server/tests/test_api_endpoints.py
@@ -2,6 +2,8 @@ import os
 import sys
 import pytest
 
+os.environ['DB_HOST'] = 'localhost'
+
 # Add src directory to path to import server module
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
